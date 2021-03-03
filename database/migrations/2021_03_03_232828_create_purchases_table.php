08f8cc8735e9d3d28a7723a12f053090182e9288
FacuBnz <facu.j.benitez@gmail.com>
2021-03-03 20:31:46 -0300

    Se agregarón nuevas migraciones

--- database/migrations/2021_03_03_232828_create_purchases_table.php
+++ database/migrations/2021_03_03_232828_create_purchases_table.php
@@ -15,8 +15,7 @@ class CreatePurchasesTable extends Migration
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('application_id')->constrained('applications')->onDelete('cascade');
+            $table->foreignId('wish_id')->constrained('wish_list')->onDelete('cascade');
             $table->timestamps();
         });
     }
