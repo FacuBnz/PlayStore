e23fd59a784be8ec88f8f4b0939aafca2f04f9a2
FacuBnz <facu.j.benitez@gmail.com>
2021-03-04 02:49:23 -0300

    Se modificó la vista home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Application;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $apps = Application::orderBy('id', 'desc')->paginate(6);
+
+        return view('home', [
+            'apps' => $apps,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,40 @@
 
 @section('content')
 <div class="container">
+    <div class="row justify-content-center mb-5">
+
+        @foreach($apps as $app)
+            <?php $sum = 0 ?>
+
+            <div class="card mr-2 mb-3 text-center shadow" style="width: 18rem;">
+                <img src="" style="max-width: 300px;" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $app->name }}</h5>
+
+                    @foreach ($app->prices as $price)
+                        @if($price->application_id == $app->id)
+                            <p class="card-text text-muted">Price: ${{ $price->price }}</p>
+                            @continue;
+                        @endif
+                    @endforeach
+
+                    @foreach ($app->califications as $calification)
+                        @if($calification->application_id == $app->id)
+                            <?php $sum += $calification->calification; ?>
+                        @endif
+                    @endforeach
+                    <?php $prom = $sum / count($app->califications); ?>
+
+                    <p class="card-text text-muted">Calification:{{ $prom }}</p><hr>
+
+                    <a href="#" class="btn btn-success">Comprar</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            
-        </div>
+        <p>{{ $apps->links() }}</p>
     </div>
 </div>
 @endsection
